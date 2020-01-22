6c33046c6871cde2f831dffd4a1157a06d374295
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2020-01-21 19:14:28 -0700

    adding some hw

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,17 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    ''' Find the lowest element, and switch places with its place in the sequence '''
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+    for i in range(len(arr)-1):
+        min_pos  = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min_pos]:
+                min_pos = j
+        temp = arr[i]
+        arr[i] = arr[min_pos]
+        arr[min_pos] = temp
 
-
-
-        # TO-DO: swap
-
-
+        print(arr)
 
 
     return arr
@@ -20,6 +19,12 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    ''' Highly Optimized Code From Lecture '''
+    for i in range(len(arr)):
+        for j in range(0, n-i-1):
+            print(arr)
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
@@ -28,3 +33,32 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+
+
+def insertion_sort(arr):
+    # split the list into sorted and unsorted
+    # For each element in unsorted...
+    counter = 0
+    for i in range(1, len(arr)):
+        print(arr)
+        # Insert that element into the correct place in a sorted by:
+        # Store the element in a temp variable
+        temp = arr[i]
+        # Shifting all larger sorted elements to the right by 1
+        j = 1
+        while j > 0 and temp < arr[j - 1]:
+            counter += 1
+            arr[j] = arr[j-1]
+            j -= 1
+        # insert the element after the first smaller element 
+        arr[j] = temp
+        print(f"counter: {counter}")
+    return arr 
+
+
+import random
+l = [7,4,9,2,6,3,0,8,5,1]
+#random.shuffle(l)
+insertion_sort(l)
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -31,3 +31,44 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+
+
+''' Class Notes on Recursion '''
+def exponent(base, power):
+    print(power)
+    if power == 0:
+        return 1
+    elif power == 1:
+        return base
+    return base * exponent(base, power-1)
+
+def exponent_iterative(base, power):
+    total = base
+    for i in range(power - 1):
+        total *= base
+    return total
+
+''' Anything you can solve recursively, you can
+    solve iteratively
+'''
+    
+# Quick Sort
+# 1. Pick a pivot and move it until everthing on the left is smaller,
+# everything on the right is greater 
+# 2. REcursively quicksort the left half, then the right half 
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    smaller = []
+    larger = []
+    for i in range(len(arr) - 1):
+        element = arr[i]
+        if element < pivot:
+            smaller.append(element)
+        else:
+            # you have to TRUST that its returning the sorted list
+    return quicksort(smaller) + [pivot] + quicksort(larger)
